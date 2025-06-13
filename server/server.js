4500bfaa409e9a2206f99995b62dfe6117577721
Pedro Aquiles Vieira Soares <54330664+pedroaquiles@users.noreply.github.com>
2025-06-13 12:08:13 -0300

    Add files via upload

--- server/server.js
+++ server/server.js
@@ -4,15 +4,13 @@ const cors = require('cors');
 
 const app = express();
 
-// Permitir CORS na origem do navegador
 app.use(cors());
 
-// Proxy para a API
 app.use('/api', createProxyMiddleware({
   target: 'https://segmarket-dash-sandbox-api.azuremicroservices.io/',
   changeOrigin: true,
   pathRewrite: {
-    '^/api': '' // remove "/api" do caminho final
+    '^/api': '' 
   }
 }));
 
